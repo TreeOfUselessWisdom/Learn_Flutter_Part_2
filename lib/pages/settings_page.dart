530e2f3238899acee73a89a859b9bbc8acf49c84
TreeOfUselessWisdom <treeofuselesswisdom@gmail.com>
2025-08-16 10:59:18 +0500

    try

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -8,21 +8,25 @@ class SettingsPage extends StatefulWidget {
 class _SettingsPageState extends State<SettingsPage> {
   TimeOfDay? time;
   DateTime? date;
+  TextEditingController ouiouiController = TextEditingController();
+  bool hide = true;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold (
-      appBar : AppBar(
-        title : const Text ('Settings'),
-        actions : [
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text ('Settings'),
+        actions: [
           PopupMenuButton(
             onSelected: (value) {
 
             },
 
-              itemBuilder: (context) => [
-            const PopupMenuItem(child: Text('Option 1')),
-            const PopupMenuItem(child: Text('Option 2')),
-          ],
+            itemBuilder: (context) =>
+            [
+              const PopupMenuItem(child: Text('Option 1')),
+              const PopupMenuItem(child: Text('Option 2')),
+            ],
           ),
         ],
       ),
@@ -30,17 +34,43 @@ class _SettingsPageState extends State<SettingsPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            // TEXT FIELD
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: TextField(
+                controller: ouiouiController,
+                obscureText: hide, // hides input (good for password fields)
+                decoration: InputDecoration(
+                  border: const OutlineInputBorder(),
+                  labelText: 'What is at the end of the Rainbow?',
+                  hintText: 'Riddle',
+                  prefixIcon: const Icon(Icons.text_fields),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      hide ? Icons.visibility_off : Icons.visibility,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        hide = !hide; // toggle visibility
+                      });
+                    },
+                  ),
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () async {
-              final t = await showTimePicker(
+                final t = await showTimePicker(
 
-                context: context,
-                initialTime: TimeOfDay.now(),
-              );
-              if (t != null) {
-                setState(() => time = t);
-              }
-            },
+                  context: context,
+                  initialTime: TimeOfDay.now(),
+                );
+                if (t != null) {
+                  setState(() => time = t);
+                }
+              },
               child: Text(time == null
                   ? 'Pick Time'
                   : time!.format(context)),
@@ -65,7 +95,8 @@ class _SettingsPageState extends State<SettingsPage> {
                   : "${date!.day}/${date!.month}/${date!.year}"),
             ),
           ],
-        ), 
+        ),
       ),
     );
   }
+}
